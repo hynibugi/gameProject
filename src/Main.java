7981f0374f82804de249eca4a5363284f3d76162
GGG <GGG@DESKTOP-8N3GG4O>
2023-01-04 14:45:53 +0900

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,9 @@
-import java.util.Locale;
-
-import com.github.javafaker.Faker;
-
 public class Main {
 	public static void main(String[] args) {
-		Faker faker = new Faker(Locale.KOREAN);
-		System.out.println(faker.name().fullName());
-		System.out.println(faker.address().fullAddress());
-
-		System.out.println(faker.pokemon().name());
+//		Faker faker = new Faker(Locale.KOREAN);
+//		System.out.println(faker.name().fullName());
+//		System.out.println(faker.address().fullAddress());
+//
+//		System.out.println(faker.pokemon().name());
 	}
 }

be0d9412da207a30d73df9032442bd6d5e0b6aab
가영 <GGG@DESKTOP-8N3GG4O>
2023-01-10 11:49:34 +0900

    게임 클래스 코드 정리

--- src/Frames/showGame.java
+++ src/Frames/showGame.java
@@ -1,6 +1,5 @@
 package Frames;
-
-import java.awt.Color;
+import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.Image;
 import java.awt.Toolkit;
@@ -18,18 +17,16 @@ import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.SwingConstants;
 import javax.swing.Timer;
 import javax.swing.border.EmptyBorder;
 
 public class showGame extends JFrame {
-	MyComponent compo = new MyComponent();
+	
 	Huddle huddle = new Huddle();
 	ClassLoader classLoader = getClass().getClassLoader();
-	List<MyComponent> list = new ArrayList<>();
 	List<Huddle> huddlelist = new ArrayList<>();
 	Toolkit kit = Toolkit.getDefaultToolkit();
-	Image imC = kit.getImage(classLoader.getResource("original.png"));
+	Image imC = kit.getImage(classLoader.getResource("stand.png"));
 	Image imbg = kit.getImage(classLoader.getResource("background.png"));
 	Image pepper = kit.getImage(classLoader.getResource("pepper.png"));
 
@@ -42,23 +39,20 @@ public class showGame extends JFrame {
 	private JLabel lbl_score;
 	private Timer timer; // 배경 타이머
 
-	private int x;
+	private int x = 0;
 	static final int BLACK = -16777216;
 	private Timer step;
 	private Timer down;
-
+	
 	public int getWhereX() {
 		return whereX;
 	}
-
 	public void setWhereX(int whereX) {
 		this.whereX = whereX;
 	}
-
 	public int getWhereY() {
 		return whereY;
 	}
-
 	public void setWhereY(int whereY) {
 		this.whereY = whereY;
 	}
@@ -70,7 +64,6 @@ public class showGame extends JFrame {
 			int ww = img.getRGB(i, img.getHeight() - 15);
 
 			if (ww == -1237980) {
-
 				huddle = new Huddle();
 				huddlelist.add(huddle);
 				huddle.setBounds(i * 10, 320, 50, 50);
@@ -83,16 +76,17 @@ public class showGame extends JFrame {
 		step = new Timer(80, new ActionListener() { // 이동속도 변경
 			@Override
 			public void actionPerformed(ActionEvent e) {
-
 				for (Huddle j : huddlelist) {
 					j.setLocation(j.getX() - 10, j.getY()); // x값-10하기
-					if (j.getX() == 120 && 280 == getWhereY()) {
-						j.setVisible(false);
-						step.stop();
-						timer.stop();
-						score.stop();
+					for (int i = 45 ; i < 84 ; i++) {
+						if (j.getX() == i && 280 == getWhereY()) {
+							System.out.println(j.getX() +" // "+ getWhereY());
+							j.setVisible(false);
+							step.stop();
+							timer.stop();
+							score.stop();
+						}
 					}
-
 				}
 			}
 		});
@@ -100,23 +94,18 @@ public class showGame extends JFrame {
 
 	}
 
-	public void showJelly() {
-
-	}
-
 	public showGame() {
 		contentPnl = new JPanel();
 		setContentPane(contentPnl);
-		contentPnl.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPnl.setBorder(null);
 		contentPnl.setLayout(null);
-
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 701, 437);
 
 		JLabel bgIng = new JLabel(""); // 배경
 		bgIng.setIcon(new ImageIcon(imbg));
 		bgIng.setBounds(0, 0, 100000, 400);
-		x = 0;
+		
 		timer = new Timer(25, new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -127,7 +116,6 @@ public class showGame extends JFrame {
 					// resultPanel.setVisible(true);
 				} else {
 					bgIng.setLocation(x, 0);
-
 				}
 			}
 		});
@@ -137,9 +125,9 @@ public class showGame extends JFrame {
 		whereX = 50;
 		whereY = 280;
 		characterIng.setFocusable(true);
-		characterIng.setBounds(whereX, whereY, 350, 153);
+		characterIng.setIcon(new ImageIcon(imC));
+		characterIng.setBounds(whereX, whereY, 68, 80);
 		characterIng.addKeyListener(new KeyListener() {
-			private int count = 0;
 
 			@Override
 			public void keyTyped(KeyEvent e) {
@@ -151,26 +139,14 @@ public class showGame extends JFrame {
 
 			@Override
 			public void keyPressed(KeyEvent e) {
-				int key = e.getKeyCode();
-
-				if (key == KeyEvent.VK_UP) {
+				down.start();
+				if (e.getKeyCode() == KeyEvent.VK_UP) {
 					whereY = 220;
-					characterIng.setBounds(whereX, whereY, 100, 100);
-					count++;
-					if (count == 1) {
-//                  System.out.println("1단점프");
-						System.out.println("(x, y) = " + getWhereX() + ", " + getWhereY());
-
-					}
-// 					자동내려오기
+					characterIng.setBounds(whereX, whereY, 68, 80);
 				}
-
-				if (key == KeyEvent.VK_DOWN) {
-//               System.out.println("슬라이드");
-					count = 0;
+				if (e.getKeyCode() == KeyEvent.VK_DOWN) {
 					whereY = 280;
-					characterIng.setBounds(whereX, whereY, 100, 100);
-//               System.out.println("(x, y) = " + getWhereX() + ", " + getWhereY());
+					characterIng.setBounds(whereX, whereY, 68, 80);
 				}
 			}
 		});
@@ -180,17 +156,13 @@ public class showGame extends JFrame {
 				if (whereY == 220) {
 					setWhereX(50);
 					setWhereY(280);
-					characterIng.setBounds(whereX, whereY, 100, 100);
+					characterIng.setBounds(whereX, whereY, 68, 80);
 					System.out.println("자동 내려오기");
-				} else {
 					down.stop();
-				}
+				} 
 			}
 		});
-		down.start();
-		;
 
-		characterIng.setIcon(new ImageIcon(imC));
 		try {
 			grapPix();
 		} catch (IOException e1) {
@@ -198,7 +170,6 @@ public class showGame extends JFrame {
 		}
 
 		lbl_score = new JLabel(String.valueOf(score1));
-
 		lbl_score.setFont(new Font("맑은 고딕", Font.BOLD, 15));
 		lbl_score.setBounds(583, 10, 90, 31);
 
@@ -209,15 +180,12 @@ public class showGame extends JFrame {
 				System.out.println("10점씩 증가");
 				System.out.println(score1);
 				score1 += 10;
-
 				lbl_score.setText(String.valueOf(score1));
-
 			}
 		});
 		score.start();
 
 		contentPnl.add(lbl_score);
-		characterIng.setIcon(new ImageIcon(imC));
 		contentPnl.add(characterIng);
 		contentPnl.add(bgIng);
 
